b9028eb54167995354ecc66420b8322cc6c00d33
Dylan <Callmeflarrow2008@gmail.dvusd.org>
2023-01-04 14:02:13 -0700

    wpf app

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Net.Http.Headers
+
+Class MainWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        TextBox1.Text = "hello world"
+    End Sub
+End Class
